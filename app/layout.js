4813b80dd115f6c4c86ed9f973fcb2a11c41f15d
Rashed Al-Masri <r7i8ta@gmail.com>
2024-10-24 11:09:06 +0300

    Add the sign-in

--- app/layout.js
+++ app/layout.js
@@ -28,17 +28,7 @@ export default function RootLayout({ children }) {
   return (
     <ClerkProvider>
       <html lang='en'>
-        <body
-          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-        >
-          <SignedOut>
-            <SignInButton />
-          </SignedOut>
-          <SignedIn>
-            <UserButton />
-          </SignedIn>
-          {children}
-        </body>
+        <body>{children}</body>
       </html>
     </ClerkProvider>
   );
--- app/sign-in/[[...sign-in]]/page.js
+++ app/sign-in/[[...sign-in]]/page.js
@@ -1,5 +1,21 @@
 import { SignIn } from '@clerk/nextjs';
+import Image from 'next/image';
 
 export default function Page() {
-  return <SignIn />;
+  return (
+    <div className='relative w-screen h-screen overflow-hidden'>
+      {/* Background Image */}
+      <Image
+        src='/uber-bg.jpg'
+        alt='bg'
+        width={900}
+        height={1000}
+        className='object-cover w-full h-full'
+      />
+      {/* Sign-In component */}
+      <div className='absolute right-5 top-20'>
+        <SignIn />
+      </div>
+    </div>
+  );
 }
